6c5957a1d3769f837e610a3021062291202b5800
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2024-11-21 19:28:18 -0300

    completions con conversacion de audio

--- audio_output.py
+++ audio_output.py
@@ -3,6 +3,9 @@ import base64
 
 client = OpenAI()
 
+messages = [{"role": "system", "content": "Responde en máximo 20 palabras, y siempre en español."}, {
+    "role": "user", "content": "Que lugar me recomiendas para vacacionar?"}]
+
 response = client.chat.completions.create(
     model="gpt-4o-audio-preview",
     modalities=["text", "audio"],
@@ -10,12 +13,40 @@ response = client.chat.completions.create(
         "voice": "sage",
         "format": "mp3"
     },
-    messages=[{"role": "system", "content": "Responde en máximo 20 palabras, y siempre en español."}, {
-        "role": "user", "content": "Que lugar me recomiendas para vacacionar?"}]
+    messages=messages
 )
 
-print(response.choices[0].message.audio.transcript)
-audio_data = response.choices[0].message.audio.data
+audio = response.choices[0].message.audio
+print(audio.transcript)
+audio_data = audio.data
 
 with open("audio_output.response1.mp3", "wb") as f:
+    f.write(base64.b64decode(audio_data))
+
+messages.append({
+    "role": "assistant",
+    "audio": {
+        "id": audio.id
+    }
+})
+messages.append({
+    "role": "user",
+    "content": "Y dentro de Chile?"
+})
+
+response = client.chat.completions.create(
+    model="gpt-4o-audio-preview",
+    modalities=["text", "audio"],
+    audio={
+        "voice": "sage",
+        "format": "mp3"
+    },
+    messages=messages
+)
+
+audio = response.choices[0].message.audio
+print(audio.transcript)
+audio_data = audio.data
+
+with open("audio_output.response2.mp3", "wb") as f:
     f.write(base64.b64decode(audio_data))

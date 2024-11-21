c689b265e1f4940e95eda06a013838577714a9e7
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2024-11-21 19:51:12 -0300

    stt response_formats

--- stt.py
+++ stt.py
@@ -2,10 +2,53 @@ from openai import OpenAI
 
 client = OpenAI()
 
+
 response = client.audio.transcriptions.create(
     model="whisper-1",
     file=open("audio_input.response1.mp3", "rb"),
-    prompt="Usaré algunas siglas como APIs y LLMs"
+    prompt="Usaré algunas siglas como APIs y LLMs",
+    response_format="text"
 )
 
 print(response)
+print("#####################")
+
+response = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=open("audio_input.response1.mp3", "rb"),
+    prompt="Usaré algunas siglas como APIs y LLMs",
+    response_format="srt"
+)
+
+print(response)
+print("#####################")
+
+response = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=open("audio_input.response1.mp3", "rb"),
+    prompt="Usaré algunas siglas como APIs y LLMs",
+    response_format="json"
+)
+
+print(response)
+print("#####################")
+
+response = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=open("audio_input.response1.mp3", "rb"),
+    prompt="Usaré algunas siglas como APIs y LLMs",
+    response_format="vtt"
+)
+
+print(response)
+print("#####################")
+
+response = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=open("audio_input.response1.mp3", "rb"),
+    prompt="Usaré algunas siglas como APIs y LLMs",
+    response_format="verbose_json"
+)
+
+print(response.to_json())
+print("#####################")
